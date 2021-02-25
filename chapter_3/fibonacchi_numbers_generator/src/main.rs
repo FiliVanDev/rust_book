b20423b4b4b5c0f3993e20c55297222e06dc0f5c
FiliVanDev <filivandev@gmail.com>
2021-02-25 15:09:16 +1200

    Add recursive and dinamic programming method

--- chapter_3/fibonacchi_numbers_generator/src/main.rs
+++ chapter_3/fibonacchi_numbers_generator/src/main.rs
@@ -1,3 +1,42 @@
+use std::io; // Use for read input
+use std::io::Write; // Use for print and input in line
+    
 fn main() {
-    println!("Hello, world!");
+    println!("Programm return value of n-Fibonacchi number");
+    loop {
+        print!("Please input number: ");
+        io::stdout().flush().unwrap(); 
+        let mut n = String::new();
+        io::stdin()
+            .read_line(&mut n)
+            .expect("Faild to read line");
+        let n: u32 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Recursive method: {}", rec_fib(n));
+        println!("Dynamic programming method: {}", dinamic_fib(n));
+    }
+
+
 }
+
+fn rec_fib(n: u32) -> u64 {
+    if n > 2 {
+        rec_fib(n-2) + rec_fib(n-1)
+    } else {
+        1
+    }
+}
+
+fn dinamic_fib(n: u32) -> u64 {
+    let mut a: u64 = 0;
+    let mut b: u64 = 1;
+    for _ in 0..n {
+        let t = b;
+        b = a+b;
+        a = t;
+    }
+    a
+}
+
